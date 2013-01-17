b4c3754195c679f4839f3045d290d9f554f4c0bf
Karan Thaker <karan.thaker@outlook.com>
2013-01-16 23:58:33 -0500

    initial commit

---
+++ SimpleMessageClient.java
@@ -0,0 +1,59 @@
+/*
+Karan Thaker
+MDB Test Client
+*/
+
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Queue;
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
+import javax.annotation.Resource;
+
+
+public class SimpleMessageClient {
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/Queue")
+    private static Queue queue;
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Session session = null;
+        MessageProducer messageProducer = null;
+        TextMessage message = null;
+        final int NUM_MSGS = 3;
+
+        try {
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            messageProducer = session.createProducer(queue);
+            message = session.createTextMessage();
+
+            for (int i = 0; i < NUM_MSGS; i++) {
+                message.setText("This is message " + (i + 1));
+                System.out.println("Sending message: " + message.getText());
+                messageProducer.send(message);
+            }
+
+            System.out.println("To see if the bean received the messages,");
+            System.out.println(
+                    " check <install_dir>/domains/domain1/logs/server.log.");
+        } catch (JMSException e) {
+            System.out.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            } // if
+
+            System.exit(0);
+        } // finally
+    } // main
+} // class
